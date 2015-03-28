da8d33086df74ced645c54dc1f901cc049cc20eb
John Uiterwyk <jnuiterwyk@gmail.com>
2015-03-29 01:29:53 +1100

    switch to use OpenCVWindow class and setup ground truth comparison

--- opencv-poc/main.cpp
+++ opencv-poc/main.cpp
@@ -1,48 +1,93 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "OpenCVWindow.h"
 #include <iostream>
+#include <cmath>
+#include <cstdlib>
 
 using namespace std;
 using namespace cv;
 
-Mat source_mat; Mat output_mat;
-char window_name1[] = "Unprocessed Image";
-char window_name2[] = "Processed Image";
+Mat source_mat, truth_mat, output_mat;
+OpenCVWindow sourceWindow("Unprocessed Image",300,300);
+OpenCVWindow testWindow("Current Image",300,300);
+OpenCVWindow leaderWindow("Leader Image",300,300);
 
 int main( int argc, char** argv )
 {
-
-    //create a window and add the image
-    namedWindow( window_name1, WINDOW_AUTOSIZE );
-    moveWindow(window_name1,0,0);
+    //timer code:
+//    double readTime = (double)getTickCount();
+//    readTime = ((double)getTickCount() - readTime)/getTickFrequency();
 
     /// Load the source image
-    double readTime = (double)getTickCount();
     source_mat = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
-    readTime = ((double)getTickCount() - readTime)/getTickFrequency();
-    cout << "Read time: " << readTime << " seconds." << endl;
+    truth_mat = imread( argv[2], CV_LOAD_IMAGE_GRAYSCALE );
 
-    imshow(window_name1,source_mat);
+    //create a window and add the image
+    testWindow.setPosition(source_mat.cols,0);
+    leaderWindow.setPosition(source_mat.cols*2,0);
+
+    sourceWindow.showImage(source_mat);
 
     //start runtime timer
     double filterTime = (double)getTickCount();
 
-    //blur the image
-    Mat blur = source_mat.clone();
-    GaussianBlur( source_mat, blur, Size( 45, 45 ), 0, 0 );
+    int top = 0;
+    for(;;)
+    {
 
-    //Mat sobelx;
-    //Sobel(blur, sobelx, CV_32F, 1, 0);
+        int i = rand();
 
-    //simple threshhold
-    Mat img_bw = blur > 180 ;
-    output_mat =  cv::Scalar::all(255) - img_bw;
+//        switch(rand()%3)
+//        {
+//            case 0:
+//                ;
+//
+//        }
 
-    //overlay on the source
-    Mat dst2;
-    double alpha = 0.5; double beta;
-    beta = ( 1.0 - alpha );
-    addWeighted( source_mat, alpha, output_mat, beta, 0.0, dst2);
+        //blur the image
+        Mat blur = source_mat.clone();
+        GaussianBlur( source_mat, blur, Size( rand()%45*2+1, rand()%45*2+1 ), 0, 0 );
+
+        //Mat sobelx;
+        //Sobel(blur, sobelx, CV_32F, 1, 0);
+
+        //simple threshhold
+        Mat img_bw = blur > rand()%255 ;
+        output_mat =  img_bw;
+
+        //overlay on the source
+        Mat dst2;
+        double alpha = 0.5; double beta;
+        beta = ( 1.0 - alpha );
+        addWeighted( source_mat, alpha, output_mat, beta, 0.0, dst2);
+
+        //show test image
+        testWindow.showImage(dst2);
+
+
+        //output score
+        Mat diff_mat;
+        cv::compare(truth_mat, output_mat, diff_mat, cv::CMP_EQ);
+        int score =  cv::countNonZero(diff_mat);
+        if(score >= top)
+        {
+            cout << "score is "<< score << endl;
+            top = score;
+            //overlay on the source
+            Mat leader_mat;
+            addWeighted( truth_mat,0.5 ,cv::Scalar::all(255) - diff_mat, 0.5, 0.0, leader_mat);
+
+
+            leaderWindow.showImage(leader_mat);
+        }
+
+
+        //create second window and output result
+       if( waitKey(16) != -1)
+           break;
+    }
+
 
     //stop runtime timer
     filterTime = ((double)getTickCount() - filterTime)/getTickFrequency();
@@ -50,13 +95,8 @@ int main( int argc, char** argv )
 
     //cout << "Tick frequency "<< std::fixed  << getTickFrequency() <<endl;
 
-    //create second window and output result
-    namedWindow( window_name2, WINDOW_AUTOSIZE );
-    imshow(window_name2,dst2);
-    moveWindow(window_name2,source_mat.cols,0);
 
 
     //wait for user to press a key
-    waitKey();
     return 0;
 }
